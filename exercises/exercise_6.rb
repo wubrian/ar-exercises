1b719e5deac3fde0f3d0c0296cc6ed26d8dce02d
Brian Wu <wubrian1234@hotmail.com>
2019-01-15 22:27:05 +0000

    complete ar assignment

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Brian", last_name: "Wu", hourly_rate: 100)
+@store1.employees.create(first_name: "Negar", last_name: "Amiri", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Chris", last_name: "Probyn", hourly_rate: 80)
+@store2.employees.create(first_name: "Gio", last_name: "Rheumer", hourly_rate: 70)
+@store2.employees.create(first_name: "Dan", last_name: "Amiri", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+Store69 = gets.chomp
+store69 = Store.create(name: Store69)
+puts store69.errors.messages
